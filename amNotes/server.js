d6123eeb36cb9ce42a65dc4bcb36865772a28e20
Raj Prasad <rajeshwarrprasad@yahoo.com>
2018-03-09 13:53:32 +1300

    Create user in db

--- amNotes/server.js
+++ amNotes/server.js
@@ -7,23 +7,50 @@ mongoose.connect('mongodb://admin:password123@ds261678.mlab.com:61678/todo');
 
 var Todo = mongoose.model('Todo', {
     text : {
-        type : String
+        type : String,
+        required : true,
+        minlength: 1,
+        trim:true
 
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
-var newTodo = new Todo ({
-    text: 'Cook dinner'
+var User = mongoose.model('User', {
+    email : {
+        type: String,
+        required: true,
+        trim: true,
+        minLength : 1
+    }
+
+});
+
+/* var newTodo = new Todo ({
+    text: '',
+    completed : false,
+    completedAt : 2018
 });
 
 newTodo.save().then((doc) => {
     console.log('Saved Todo', doc);
 }, (e) => {
     console.log('Unable to save todo')
+}); */
+
+var newUser = new User({
+    email: 'myemail@yahoo.com'
 });
+
+// newUser.save().then((doc) => {
+//     console.log('Saved User', doc);
+// }, (e) => {
+//     console.log('Unable to save user')
+// });
